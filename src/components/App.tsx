67e66d61ff260875aa7aa463917a07b47870eea2
Marcos Felipe A. Ferreira <marcos.fel93@gmail.com>
2022-01-15 18:34:33 -0300

    Install&Setup react router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 
-function App() {
+const Menu = () => (
+    <>
+        <Link to="/">Home page</Link><br/>
+        <Link to="/about">About page</Link>
+    </>
+)
+
+const Home = () => (
+    <>
+        <h2>Home page</h2>
+        <Menu />
+    </>
+)
+
+const About = () => (
+    <div>
+        <h2>About page</h2>
+        <Menu />
+    </div>
+)
+
+const App = () => {
   return (
-    <div className="App">Hello</div>
+    <div className="App">
+      <h1>Hello</h1>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
   );
 }
 
