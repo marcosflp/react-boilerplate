62adfa002ddec735096c055eded9c18c5e6c59f9
Marcos Felipe A. Ferreira <marcos.fel93@gmail.com>
2022-01-15 20:29:49 -0300

    Install styled-components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,43 +2,120 @@ import React from 'react';
 import * as R from 'ramda';
 import { Routes, Route, Link } from 'react-router-dom';
 import { gql, useQuery } from "@apollo/client";
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: left;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+const Container = styled.div`
+  width: 980px;
+  margin: 0 auto;
+`;
+
+const MenuItem = styled.button`
+  margin-right: 1rem;
+  appearance: none;
+  backface-visibility: hidden;
+  background-color: #2f80ed;
+  border-radius: 10px;
+  border-style: none;
+  box-shadow: none;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  display: inline-block;
+  font-family: Inter,-apple-system,system-ui,"Segoe UI",Helvetica,Arial,sans-serif;
+  font-size: 15px;
+  font-weight: 500;
+  height: 50px;
+  letter-spacing: normal;
+  line-height: 1.5;
+  outline: none;
+  overflow: hidden;
+  padding: 14px 30px;
+  position: relative;
+  text-align: center;
+  text-decoration: none;
+  transform: translate3d(0, 0, 0);
+  transition: all .3s;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  vertical-align: top;
+  white-space: nowrap;
+  
+  &:hover {
+    background-color: #1366d6;
+      box-shadow: rgba(0, 0, 0, .05) 0 5px 30px, rgba(0, 0, 0, .05) 0 1px 4px;
+      opacity: 1;
+      transform: translateY(0);
+      transition-duration: .35s;
+  }
+  
+  &:active {
+    box-shadow: rgba(0, 0, 0, .1) 0 3px 6px 0, rgba(0, 0, 0, .1) 0 0 10px 0, rgba(0, 0, 0, .1) 0 1px 4px -1px;
+    transform: translateY(2px);
+    transition-duration: .35s;
+  }
+
+  @media (min-width: 768px) {
+    .button-65 {
+      padding: 14px 22px;
+      width: 176px;
+    }
+}
+`
 
 const Menu = () => (
     <>
-        <Link to="/">Home page</Link><br/>
-        <Link to="/about">About page</Link>
+        <Link to="/"><MenuItem>Home page</MenuItem></Link>
+        <Link to="/about"><MenuItem>About page</MenuItem></Link>
     </>
 )
 
 const Home = () => (
     <>
-        <h2>Home page</h2>
-        <Menu />
+        <Wrapper>
+            <Menu />
+        </Wrapper>
+
+        <Container>
+            <Title>Home page</Title>
+        </Container>
     </>
 )
 
 const About = () => {
     const { data, loading } = useQuery(GET_RATES_QUERY)
 
-    console.log(data)
-
     return (
         <div>
-            <h2>About page</h2>
-            <Menu />
+            <Wrapper>
+                <Menu />
+            </Wrapper>
 
-            <h3>Data</h3>
-            <div>
-                {loading ? ("Loading") : (
-                    <>
-                        {R.map((rate) => (
-                            <div key={rate}>
-                                {rate.currency}
-                            </div>
-                        ), data.rates)}
-                    </>
-                )}
-            </div>
+            <Container>
+                <Title>About page</Title>
+                <div>
+                    {loading ? ("Loading") : (
+                        <>
+                            {R.map((rate) => (
+                                <div key={rate.currency}>
+                                    {rate.currency}
+                                </div>
+                            ), data.rates)}
+                        </>
+                    )}
+                </div>
+            </Container>
         </div>
     )
 }
@@ -46,8 +123,6 @@ const About = () => {
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello</h1>
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
