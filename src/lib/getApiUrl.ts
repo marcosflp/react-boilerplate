c363dc288db877a98cc43c5a0506b67f6d462ffd
Marcos Felipe A. Ferreira <marcos.fel93@gmail.com>
2022-01-17 22:10:11 -0300

    Fix env

--- src/lib/getApiUrl.ts
+++ src/lib/getApiUrl.ts
@@ -1,8 +1,8 @@
 import { isEnvProduction } from './enviroment';
 
 const getApiUrl = (): string => {
-  if (isEnvProduction() && process.env.BACKEND_API_URL) {
-    return process.env.BACKEND_API_URL;
+  if (isEnvProduction() && process.env.REACT_APP_BACKEND_API_URL) {
+    return process.env.REACT_APP_BACKEND_API_URL;
   }
   // Return localhost API URL
   return 'http://localhost:8000/graphql';
